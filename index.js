79c50f7a52cb7eb1e2c54833e8f1723eba953dd5
BerberKagan <kaganberbero@gmail.com>
2023-02-01 18:33:34 +0300

    [200~Checked off my finished tasks

--- index.js
+++ index.js
@@ -14,10 +14,27 @@
         + Bu `isim` ve `yas` i içeren bir string döndürmelidir Örnek: "Mary, 50"
 */
 
-function Kisi(/* kodlar buraya */) {
-  /* kodlar buraya */
+function Kisi(isim, yas) {
+  this.isim = isim;
+  this.yas = yas;
+  this.mide = [];
+  this.ye = function(yemek) {
+    this.mide.push (yemek);
+    if (this.mide.length<11) {
+      this.mide.push(yemek);
+    }
+  }
+  this.bosalt = function () {
+    this.mide = [];
+  }
+  this.toString = function () {
+    return `${this.isim}, ${this.yas}`;
+  }
 }
-
+let name = new Kisi ("İhsan", 40)
+name.ye("Patates")
+name.ye("Hamburger")
+console.log(name.mide);
 
 /*
   GÖREV 2
@@ -35,18 +52,40 @@ function Kisi(/* kodlar buraya */) {
         +  "x milde benzinim bitti!" x değişkeni `odometer` daki sayı olmalıdır.
 */
 
-function Araba(/* kodlar buraya */) {
-  /* kodlar buraya */
+function Araba(model, tuketim) {
+  this.model = model;
+  this.milesPerGallon = tuketim;
+  this.tank = 0;
+  this.odometer = 0; 
+  this.fill = function (galon) {
+    this.tank+=galon; 
+  }
+  this.drive = function (distance) {
+    this.odometer+=distance;
+    this.tank-=Math.round(distance/this.milesPerGallon);
+    if (this.tank<=0) {
+      const extraMile = this.tank * this.milesPerGallon;
+      this.odometer+=Math.round(extraMile);
+      this.tank = 0;
+      console.log (`${this.odometer} milde benzinim bitti!`)
+    } 
+  }
 }
+let vw = new Araba ("Passat", 15);
+vw.fill(65);
+vw.drive(125);
+vw.drive(950);
+console.log(vw.tank);
+console.log(vw.odometer);
 
 
 /* 
   GÖREV 3
   Kendi cümlelerinizle "this" kelimesinin 4 prensibini açıklayın:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window/Global Object Binding: Global scope’tayken, this değeri window/console nesnesi olacaktır.
+  2. Implicit Binding: Örtük bağlama, bir fonksiyonu çağırmak için nokta gösterimi kullanıldığında gerçekleşir.
+  3. New Binding: Bir constructor function(yapıcı fonksiyon) kullandığımızda, this, constructor function tarafından oluşturulan ve döndürülen nesnenin belirli bir örneğini ifade eder.
+  4. Explicit Binding: JavaScript'in call veya apply yöntemini her kullandığımızda, this açıkça tanımlanır. 
 */
 
 
